f6821a7447afff563f04523b2e988c2c82b68987
 Vladimir <chekalin@MacBook-Air-Vladimir-3.local>
2025-11-25 20:16:40 +0300

    Fix

--- Makefile
+++ Makefile
@@ -1,17 +1,17 @@
 CC=gcc
-FLAGS=-fopenmp $(OPTIMIZE)
+FLAGS=-fopenmp -$(OPTIMIZE)
 DIR=$(PWD)/build
 
 all: var714 var714_parallel var714_parallel_task
 
-var714: $(PWD)/src/var714.c
+var714: $(PWD)/src/var714_$(N)_$(OPTIMIZE).c
 	$(CC) $(FLAGS) -o $(DIR)/$@ $<
 
-var714_parallel: $(PWD)/src/var714_parallel.c
+var714_parallel: $(PWD)/src/var714_parallel_$(N)_$(OPTIMIZE).c
 	$(CC) $(FLAGS) -o $(DIR)/$@ $<
 
-var714_parallel_task: $(PWD)/src/var714_parallel_task.c
+var714_parallel_task: $(PWD)/src/var714_parallel_task_$(N)_$(OPTIMIZE).c
 	$(CC) $(FLAGS) -o $(DIR)/$@ $<
 
 clean:
-	rm -f $(DIR)/var714 $(DIR)/var714_parallel $(DIR)/var714_parallel_task
+	rm -f $(DIR)/*
--- var714_test.py
+++ var714_test.py
@@ -4,7 +4,7 @@ import subprocess
 
 
 LSF_SCRIPT = """#BSUB -n {M}
-#BSUB -W 00:15
+#BSUB -W 01:30
 #BSUB -o "outputs/{name}_{N}_{optimize}.out"
 #BSUB -e "errors/{name}_{N}_{optimize}.err"
 #BSUB -R "span[hosts=1]"
@@ -32,9 +32,9 @@ if __name__ == "__main__":
     num_threads = args.num_threads
     tasks = ["var714", "var714_parallel", "var714_parallel_task"]
     subprocess.run(["make", "clean"])
-    subprocess.run(["make", f"OPTIMIZE=-{args.optimize}"])
+    subprocess.run(["make", f"OPTIMIZE={args.optimize}", f"N={num_threads}"])
     for task in tasks:
-        script_content = LSF_SCRIPT.format(M=num_threads // 8 + 1, N=num_threads, name=task, optimize=args.optimize)
+        script_content = LSF_SCRIPT.format(M=num_threads // 8 + 1, N=num_threads, name=f"{task}_{num_threads}_{optimize}", optimize=args.optimize)
         script_path = pathlib.Path(f"{task}.lsf")
         script_path.write_text(script_content)
         subprocess.run(["bsub"], input=script_path.read_bytes())
