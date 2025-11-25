03c65376644d87773ac5ef91c1e9a377b8f8a0f2
 Vladimir <chekalin@MacBook-Air-Vladimir-3.local>
2025-11-25 20:01:31 +0300

    Fix

--- start_test.sh
+++ start_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for N in 1 2 3 4 5 6 7 8 9 10 20 40 60 80 100 120 140 160
 do
-    for optimize in "-O0" "-O1" "-O2" "-O3" "-Ofast"
+    for optimize in "O0" "O1" "O2" "O3" "Ofast"
     do
         echo "Running test with N=$N and optimize=$optimize"
         python3 var714_test.py -N $N --optimize $optimize
